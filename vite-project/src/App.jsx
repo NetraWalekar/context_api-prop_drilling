91633865123da63abecf4a203cc834e6d77e98f6
NetraWalekar <netrawalekar6@gmail.com>
2025-02-07 18:51:56 +0530

    complete

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,34 @@
-import './App.css'
+import React from 'react'
+import {BrowserRouter,Route,Routes, useNavigate} from 'react-router-dom'
+const Dashboard = React.lazy(() => import('./components/Dashboard'))
+const Landing = React.lazy(() => import('./components/Landing'))
 
 function App() {
 
+
   return (
-   <div>
     
-   </div>
+    <BrowserRouter>
+    <AppBar/>
+    <Routes>
+      <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+      <Route path='/' element={<Landing></Landing>}></Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+function AppBar(){
+  const navigate = useNavigate()
+  return(
+      <div>
+        <button onClick={() => {
+         navigate("/")
+        }}>Landing</button>
+        <button onClick={() => {
+         navigate("/dashboard")
+        }}>Dashboard</button>
+      </div>
   )
 }
 
